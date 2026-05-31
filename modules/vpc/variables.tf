f06dd8dd56111a75eccd0103d0d869275cb8aee9
rizwanshaikhcr7 <rizwanshaikhcr7@gmail.com>
2026-05-31 18:21:17 +0530

    Add VPC variables for project configuration

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Project name for tagging"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+}
