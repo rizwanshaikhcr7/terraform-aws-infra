1d3c16f415d228fae483efc94b80399b51771092
rizwanshaikhcr7 <rizwanshaikhcr7@gmail.com>
2026-05-31 18:24:00 +0530

    Add variables for EC2 module configuration

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,34 @@
+variable "project_name" {
+  description = "Project name for tagging"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be launched"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the security group"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name for SSH access"
+  type        = string
+}
